ce01e839fa6fe2b1baae7a44354f53344bff6216
RushiPandit <142237925+RushiPandit@users.noreply.github.com>
2023-09-11 00:20:46 +0530

    Update FXTradingRequests.java

--- FXTradingRequests.java
+++ FXTradingRequests.java
@@ -15,39 +15,36 @@ import com.FXTrading.services.FXTradingFormatImpl;
 @RestController
 public class FXTradingRequests {
 
-	@Autowired
-	private FXTradingFormatImpl fxTradingFormatImpl;
+    @Autowired
+    private FXTradingFormatImpl fxTradingFormatImpl;
 
-	@PostMapping(value = "/do-trade")
-	public Object doTrading(@RequestBody FXTradingEntity fxTradingEntity) {
+    @PostMapping("/do-trade")
+    public Object doTrading(@RequestBody FXTradingEntity fxTradingEntity) {
+        if (fxTradingEntity == null) {
+            return "Not Booked";
+        }
+        try {
+            Object object = fxTradingFormatImpl.doTrade(fxTradingEntity);
+            if (object instanceof Map) {
+                Map map = (Map) object;
+                map.put("Status", "Not Booked");
+                return map;
+            } else {
+                return "Trade Booked";
+            }
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
 
-		if (fxTradingEntity == null) {
-			return "Not Booked";
-		}
-		try {
-//							FXTradingEntity fxTradingEntity2=@RequestBody FXTradingEntity fxTradingEntity;
-			Object object = fxTradingFormatImpl.doTrade(fxTradingEntity);
-			if (object instanceof Map) {
-				((Map) object).put("Status", "Not Booked");
-				return object;
-			} else {
-				return "Trade Booked";
-			}
-
-		} catch (Exception e) {
-			return e.getMessage();
-		}
-
-	}
-
-	@GetMapping("/get-all-trades")
-	public Object getAllTrades() {
-		List<FXTradingEntity> tradeList = this.fxTradingFormatImpl.getTradeList();
-		if (tradeList.isEmpty()) {
-			return "Your trade List is Empty";
-		} else {
-			return tradeList;
-		}
-	}
-
+    @GetMapping("/get-all-trades")
+    public Object getAllTrades() {
+        List<FXTradingEntity> tradeList = fxTradingFormatImpl.getTradeList();
+        if (tradeList.isEmpty()) {
+            return "Your trade List is Empty";
+        } else {
+            return tradeList;
+        }
+    }
 }
+
