95a061b549e4b7f6084e1bb8a5ab5b470a4e204c
RushiPandit <142237925+RushiPandit@users.noreply.github.com>
2023-09-11 00:29:45 +0530

    Update FXTradingFormat.java

--- FXTradingFormat.java
+++ FXTradingFormat.java
@@ -5,8 +5,19 @@ import java.util.List;
 import com.FXTrading.model.FXTradingEntity;
 
 public interface FXTradingFormat {
-	
-	public Object doTrade(FXTradingEntity fxTradingEntity);
-	public List<FXTradingEntity> getTradeList();
 
+    /**
+     * Performs a trade.
+     *
+     * @param fxTradingEntity The FX trading entity.
+     * @return The result of the trade.
+     */
+    Object doTrade(FXTradingEntity fxTradingEntity);
+
+    /**
+     * Gets the list of trades.
+     *
+     * @return The list of trades.
+     */
+    List<FXTradingEntity> getTradeList();
 }
